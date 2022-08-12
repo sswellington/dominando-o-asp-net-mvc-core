17fc051894a9a64ee92011a422f79a901d3e60d3
sswellington <sswellington@pm.me>
2022-08-11 21:44:52 -0300

    feat: TagHelper - email

---
+++ AppModelo/src/Sswellington.UI.Web/Extensions/EmailTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Sswellington.UI.Web.Extensions
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string EmailDomain { get; set; } = "protonmail.com";
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a"; //html referente a tag de âncora
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + "@" + EmailDomain;
+              
+            output.Attributes.SetAttribute("href", "mailto" + target); 
+            output.Content.SetContent(target);
+        }
+    }
+}
